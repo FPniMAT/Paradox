e68ee72f359f8d7a068111132d2115f915c4debe
Floyd Pantaleon <78898754+FPniMAT@users.noreply.github.com>
2026-07-12 06:49:49 +0800

    feat: add protocol constants library

---
+++ src/libraries/Constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+library Constants {
+    string internal constant TOKEN_NAME = "Paradox";
+    string internal constant TOKEN_SYMBOL = "PRDX";
+    uint8 internal constant DECIMALS = 18;
+    uint256 internal constant MAX_BPS = 10000;
+}
